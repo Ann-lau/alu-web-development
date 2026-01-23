12796407f217013970a3b1060add11bdabd13098
Ann Lau <101356391+Ann-lau@users.noreply.github.com>
2026-01-23 18:45:25 +0300

    Handle None key/item in put() method

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -23,10 +23,12 @@ class BasicCache(BaseCaching):
     """
 
     def put(self, key, item):
-        ''' self descriptive '''
-        if key and item:
-            self.cache_data[key] = item
-
+        """ Add key-value pair to the cache """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        
+        
     def get(self, key):
         """
             modify cache data
